94e8c125f9d34c3b295b53718908eedaabc49a00
Peleg Daniel <41775219+pelegd@users.noreply.github.com>
2024-10-10 11:45:40 +0300

    Update vim-options.lua
    adding some nvim options from the from theprimagen
    https://www.youtube.com/watch?v=w7i4amO_zaE&t=1350s

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -6,11 +6,29 @@ vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.g.background = "light"
 
-vim.opt.swapfile = false
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+vim.opt.wrap = false
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
+
+vim.opt.swapfile = false
 vim.opt.conceallevel = 0                -- so that `` is visible in markdown files
-vim.opt.hlsearch = true                 -- highlight all matches on previous search pattern
 vim.opt.ignorecase = true               -- ignore case in search patterns
 
 -- Navigate vim panes better
